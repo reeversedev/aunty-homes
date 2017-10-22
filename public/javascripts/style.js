0aec84cbf2bb34fc0956a9de452ca4a419244796
Prateek Gogia <gogia.prateek@hotmail.com>
2017-10-22 19:27:14 +0530

    Added public.

---
+++ public/javascripts/style.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(".button-collapse").sideNav();
+    $('.carousel').carousel();
+    $('.parallax').parallax();
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+});
